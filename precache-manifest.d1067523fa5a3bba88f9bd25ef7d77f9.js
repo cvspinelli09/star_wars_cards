f711e834afce6b8cf51c3440adec9f6c0073517e
cvspinelli <cvspinelli09@gmail.com>
2020-01-27 13:39:43 +0000

    Updates

---
+++ precache-manifest.d1067523fa5a3bba88f9bd25ef7d77f9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdb830d08cc638bbbff7898c7420e54",
+    "url": "/star_wars_cards/index.html"
+  },
+  {
+    "revision": "228f0641f132ccf7f3e9",
+    "url": "/star_wars_cards/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8fdb341be8dd175f5b89",
+    "url": "/star_wars_cards/static/css/main.7c43ac9d.chunk.css"
+  },
+  {
+    "revision": "228f0641f132ccf7f3e9",
+    "url": "/star_wars_cards/static/js/2.6687ac5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star_wars_cards/static/js/2.6687ac5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fdb341be8dd175f5b89",
+    "url": "/star_wars_cards/static/js/main.7de22686.chunk.js"
+  },
+  {
+    "revision": "9d6e0c79f7ed7efd282c",
+    "url": "/star_wars_cards/static/js/runtime-main.46c5dc64.js"
+  },
+  {
+    "revision": "3153ea79b0cc988589827ff32ee8c3c7",
+    "url": "/star_wars_cards/static/media/C3-PO.3153ea79.jpg"
+  },
+  {
+    "revision": "db5c926a8ce1cb8aca64e276746a37d2",
+    "url": "/star_wars_cards/static/media/R2-D2.db5c926a.jpg"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/star_wars_cards/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4c5612d98d5d7cce14e8d5ae2fa6f1ca",
+    "url": "/star_wars_cards/static/media/darth-vader.4c5612d9.jpg"
+  },
+  {
+    "revision": "4b3dd76533185c89edbe70b89b97ced9",
+    "url": "/star_wars_cards/static/media/emperor.4b3dd765.jpg"
+  },
+  {
+    "revision": "850fc62f98411385bcfb03491685a47c",
+    "url": "/star_wars_cards/static/media/leia.850fc62f.jpg"
+  },
+  {
+    "revision": "6a0433e7dd9d3ada9b78405f01d163c4",
+    "url": "/star_wars_cards/static/media/lukeskywalker.6a0433e7.jpg"
+  },
+  {
+    "revision": "14d5e46c8505f50616a4f8fc9c3d60df",
+    "url": "/star_wars_cards/static/media/obi-wan.14d5e46c.jpg"
+  },
+  {
+    "revision": "8d7861d2bf38fccbf5de125e57b1be1f",
+    "url": "/star_wars_cards/static/media/yoda.8d7861d2.jpg"
+  }
+]);
